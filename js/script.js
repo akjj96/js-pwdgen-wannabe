04d22f439106a325af09b21821321eee62aa13c1
akjj96 <akimjj96@gmail.com>
2022-03-23 17:35:54 +0100

    index:lavoro

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let userName = prompt('Come ti chiami?')
+console.log(userName);
+
+let userSurname = prompt("Qual'è il tuo cognome");
+console.log(userSurname);
+
+let userFavoriteColor = prompt("Qual'è il tuo colore preferito");
+console.log(userFavoriteColor);
+
+
+
+document.getElementById('password').innerHTML 
+
+
+// a{richiesta nome}-->b[finestra dove scrivere il nome]
+// b-->c{richiesta cognome}
+// c-->e[finestra dove scrivere cognome]
+// e-->f{richiesta colore preferito}
+// f-->g[finestra dove scrivere il colore preferito]
+// g-->h[scrivere in html il mokup]
+// h-->i[fine lavoro]
